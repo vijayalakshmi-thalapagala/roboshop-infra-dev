a6b30a72c735cbfe35757dd07186e2707782d411
Vijayalakshmi Thalapagala <thalapav@amdocs.com>
2026-03-26 20:25:01 +0530

    actions

---
+++ 40-databases/bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+component=$1
+environment=$2
+dnf install ansible -y
+
+cd /home/ec2-user
+git clone https://github.com/daws-88s/ansible-roboshop-roles-tf.git
+
+cd ansible-roboshop-roles-tf
+git pull
+ansible-playbook -e component=$component -e env=$environment roboshop.yaml
